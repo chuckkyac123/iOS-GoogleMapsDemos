98fd0f4ea8f97aada807d8652e8a4d4045b6b414
chuckkyac123 <chuckkyac123@gmail.com>
2015-06-11 00:15:20 +0700

    Initial Commit

---
+++ GoogleMapsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleMapsTest
+//
+//  Created by Chuckky Bank on 6/11/15.
+//  Copyright (c) 2015 Chuckky Bank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoogleMapsTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoogleMapsTest
+//
+//  Created by Chuckky Bank on 6/11/15.
+//  Copyright (c) 2015 Chuckky Bank. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
